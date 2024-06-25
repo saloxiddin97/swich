379761ef3df184a26c6d5c1ba430f3937896f95e
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-25 11:12:14 +0500

    swich

---
+++ srript.js
@@ -0,0 +1,29 @@
+
+let a= prompt("birdan 7 gacha bolgan sonlarni kriting")
+let b=a.toLowerCase()
+switch(b){
+case"1":
+console.log("DUSHANBA" );
+break;
+case"2":
+console.log("SESHANBA" );
+break;
+case"3":
+console.log("CHORSHANBA" );
+break;
+case"4":
+console.log("PAYSHANBA" );
+break;
+case"5":
+console.log("JUMA" );
+break;
+case"6":
+console.log("SHANBA" );
+break;
+case"7":
+console.log("YAKSHANBA" );
+break;
+default:
+    console.log( "bir haftada 7 kun bor");
+   break;
+}
